185e4bda6085c3833d991839c183167e3bf9b162
Drew Marie Williams <dmariew@mimosa.cs.uchicago.edu>
2012-05-27 02:01:12 -0500

    FIXED ALL CHORD SERVER ERRORS.

--- connection/example_client.cpp
+++ connection/example_client.cpp
@@ -1,11 +1,8 @@
 /*
- *  example_client.cpp
- *  A chord-based matchmaking server client.
- *  Connects to server.cpp and can modify data in that chord.  Also
- *  continues the Chord if the initial connection goes down.
- *
- *  CMSC 23310 - Drew Williams and Rina Kelly, 2012
- *  cChord created by Laurent Vanni & Nicolas Goles Domic, 2010
+ *  example.cpp
+ *  A simple main() to try out the Library
+ *  From cChord source
+ *  Created by Laurent Vanni & Nicolas Goles Domic, 2010
  *
  */
 
@@ -18,67 +15,76 @@
 
 using namespace std;
 
-/*  need args, "ip", "port", "overlay identifier 
- *  (unique string)")" 
- */
- 
-int main(int argc, char *argv[]) 
+// This application receives args, "ip", "port", "overlay identifier (unique string)", "root directory)"
+int main(int argc, char * const argv[]) 
 {
-    string hostname = "mimosa.cs.uchicago.edu";
-
-    /* Originally "backbone" */
+	string backBone[] = {
+			// user backbone
+            "mimosa.cs.uchicago.edu",
+	};
 	
     Node *chord = NULL;
     ChordNode *node = NULL;
-    
-    //This will always join to the mimosa.cs.uchicago.edu chord.
-    cout << "client now joining...\n";
-    int i = 0;
-    node = P_SINGLETON->initChordNode(std::string(argv[2]), 
-			   atoi(argv[3]), std::string("ruemud"),std::string("."));
-    chord = new Node(hostname, 9000);
-    node->join(chord);
 
-    char entry[256];
-    string key;
-    string value;
+	if (argc >= 4) {
+		// Create a test node
+		node = P_SINGLETON->initChordNode(std::string(argv[1]), atoi(argv[2]), std::string("chordTestBed"), std::string(argv[3]));
+        chord = NULL;
+        
+		// join to an existing chord
+		if (argc == 5) {
+			cout << "joining...\n";
+			int i = 0;
+		    chord = new Node(backBone[0], 8000);
+			node->join(chord);
+		}
+
+		char entry[256];
+		string key;
+		string value;
 		
-    while (1) { // sleep...
-	cout << "\n0) Print status\n" << 
-		  "1) Put\n" << 
-	  	  "2) Get\n" <<
-	 	  "3) Remove\n" << 
-		  "4) Exit\n\n";
-	cout << "---> ";
-	cin >> entry;
-	int chx = atoi(entry);
+		while (1) { // sleep...
+			cout << "\n0) Print status\n" << 
+					"1) Put\n" << 
+					"2) Get\n" <<
+					"3) Remove\n" << 
+					"4) Exit\n\n";
+			cout << "---> ";
+			cin >> entry;
+			int chx = atoi(entry);
 
-	switch (chx) {
-    	case 0:
-    		cout << "\n" << node->printStatus();
-    		break;
-    	case 1:
-    		cout << "Key = ";
-    		cin >> key;
-    		cout << "Value = ";
-    		cin >> value;
-    		node->put(key, value);
-    		break;
-    	case 2:
-    		cout << "Key = ";
-    		cin >> key;
-    		cout << "\n" << node->get(key) << "------> found!" << endl;
-    		break;
-    	case 3:
-    		cout << "Key = ";
-    		cin >> key;
-    		node->removekey(key);
-    		break;
-    	case 4:
-    		node->shutDown();
-    		default:
-		break;
-	}       
+			switch (chx) {
+    			case 0:
+    				cout << "\n" << node->printStatus();
+    				break;
+    			case 1:
+    				cout << "Key = ";
+    				cin >> key;
+    				cout << "Value = ";
+    				cin >> value;
+    				node->put(key, value);
+    				break;
+    			case 2:
+    				cout << "Key = ";
+    				cin >> key;
+    				cout << "\n" << node->get(key) << "------> found!" << endl;
+    				break;
+    			case 3:
+    				cout << "Key = ";
+    				cin >> key;
+    				node->removekey(key);
+    				break;
+    			case 4:
+    				node->shutDown();
+    			default:
+				break;
+			}       
+	    }
+    } else {
+		cout << "wrong parameters: test.out <hostname> <portNumber> <webContentDirectory> [--join]\n";
 	}
 
-    }
+    delete node;
+    delete chord;
+	return 0;
+}
